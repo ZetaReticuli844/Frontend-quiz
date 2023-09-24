be22ffd9830945bf388d65db9081060dec7c5fb9
Yogesh <awesomeyogi10@gmail.com>
2023-09-24 19:27:18 +0530

    added Routes for all the web pages

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+const Dashboard = () => {
+    const { userId } = useParams()
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Dashboard
