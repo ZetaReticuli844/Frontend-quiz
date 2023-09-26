54df7df0cc842661d97247cd409a7a0f9f1c9525
Yogesh <awesomeyogi10@gmail.com>
2023-09-26 17:43:30 +0530

    configured routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route,Link,Routes } from 'react-router-dom'
-import Welcome from './components/Welcome'
 
+import { Route,Link,Routes,BrowserRouter } from 'react-router-dom'
+import {Quiz,CreateQuiz, Dashboard, Login, QuizList, Register, ResultQuiz,Welcome,} from './pages'
 
 const App = () => {
 
   return(
+    <BrowserRouter>
 <Routes>
-  <Route path="/" element={Welcome}/>
-  <Route path="/resgister" element={Register}/>
-  <Route path="/login" element={Login}/>
-  <Route path="/dashboard:userId" element={Dashboard}/>
-  <Route path="/create" element={CreateQuiz}/>
-  <Route path="/quiz-list" element={QuizList}/>
-  <Route path="/quiz/:quizId" element={Quiz}/>
-  <Route path="/result/:quizId" element={ResultQuiz}/>
-
-
-
-
+  <Route path="/" element={<Welcome/>}/>
+  <Route path="/resgister" element={<Register/>}/>
+  <Route path="/login" element={<Login/>}/>
+  <Route path="/dashboard:userId" element={<Dashboard/>}/>
+  <Route path="/create" element={<CreateQuiz/>}/>
+  <Route path="/quiz-list" element={<QuizList/>}/>
+  <Route path="/quiz/:quizId" element={<Quiz/>}/>
+  <Route path="/result/:quizId" element={<ResultQuiz/>}/>
 </Routes>
+</BrowserRouter>
   )
 }
 export default App
--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import AttemptQuiz from "./AttemptQuiz";
-import CreateQuiz from "./CreateQuiz";
-import Home from "./Home";
-import Login from "./Login";
-import QuizList from "./QuizList";
-import Register from "./Register";
-import Result from "./ResultQuiz";
-
-export { AttemptQuiz, CreateQuiz, Home, Login, QuizList, Register, Result };
