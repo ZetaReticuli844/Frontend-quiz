36248159e6bfe30a69b9735cf79373d1996bceec
Yogesh <awesomeyogi10@gmail.com>
2023-09-28 18:58:56 +0530

    added in contex functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
 import { Route,Link,Routes,BrowserRouter } from 'react-router-dom'
-import {Quiz,CreateQuiz, Dashboard, Login, QuizList, Register, ResultQuiz,Welcome,} from './pages'
-
+import {Quiz,CreateQuiz, Dashboard, Login, QuizList, Register, ResultQuiz,Welcome,Home} from './pages'
+import PrivateRoute from './utils/PrivateRoute'
+import { AuthProvider } from './context'
 const App = () => {
 
   return(
     <BrowserRouter>
 <Routes>
-  <Route path="/" element={<Welcome/>}/>
+<Route path="/" element={<Welcome/>}/>
+<AuthProvider>
+<Route path="/home" element={<Home/>}/>
   <Route path="/resgister" element={<Register/>}/>
   <Route path="/login" element={<Login/>}/>
   <Route path="/dashboard:userId" element={<Dashboard/>}/>
@@ -15,6 +18,7 @@ const App = () => {
   <Route path="/quiz-list" element={<QuizList/>}/>
   <Route path="/quiz/:quizId" element={<Quiz/>}/>
   <Route path="/result/:quizId" element={<ResultQuiz/>}/>
+  </AuthProvider>
 </Routes>
 </BrowserRouter>
   )
